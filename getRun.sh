7a28b8c08c3744a0ee1cdb674dab9694a9be440f
Renato Puga <renatopuga@gmail.com>
2020-12-16 14:01:04 -0300

    Update getRun.sh

--- getRun.sh
+++ getRun.sh
@@ -1,8 +1,14 @@
+# caminho do comando fastq-dump
+fastqdump="/scratch/app/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump"
+
+# criar diretorios de input e output
+mkdir -p input output
+
 # copiar, extrair, 
 for srafile in $(cat $1)
 do
     wget -c https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-7/$srafile
-    ~/rat/sratoolkit.2.10.8-ubuntu64/bin/fastq-dump --split-files $(basename $srafile)
+    $fastqdump --split-files $(basename $srafile)
     rm -rf $(basename $srafile)
     mkdir input/$(basename $srafile)
     mv $(basename $srafile)*.fastq input/$(basename $srafile)
